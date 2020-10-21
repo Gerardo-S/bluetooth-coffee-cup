3928873c37cd4675170a361d130505f8f9230958
Chris C <christopher.cruzcosa@gmail.com>
2020-10-21 12:50:18 -0700

    added main ajax calls for the rawg api

---
+++ public/js/rawgCalls.js
@@ -0,0 +1,40 @@
+let baseSearchUrl = "https://api.rawg.io/api/games?key=";
+let apiKey = "72506a506521467da93378cfb7fc9829"
+let searchString;
+const searchQueryURL = baseSearchUrl += apiKey += "&search=" += searchString += "&ordering=-rating&exclude_additions=true";
+
+let baseGameDetailsUrl = "https://api.rawg.io/api/games/";
+let gameId;
+const gameDetailsURL = baseGameDetailsUrl += gameId += "?key=" += apiKey;
+
+
+function getGameSearchResults(searchString) {
+    searchString = searchString;
+
+    $.ajax({
+        url: searchQueryURL,
+        method: "GET",
+      }).then(function (data) {
+        return data
+      })
+      .catch((err) => {
+        console.log(error)
+      })
+};
+
+function getGameDetails(gameId) {
+    gameId = gameId;
+
+    $.ajax({
+        url: gameDetailsURL,
+        method: "GET",
+      }).then(function (data) {
+        return data
+      })
+      .catch((err) => {
+        console.log(error)
+      })
+};
+
+exports.getGameSearchResults = getGameSearchResults;
+exports.getGameDetails = getGameDetails;
