3e560e842dbe092611d2f92a3b7fae337d8b9a17
Chris C <christopher.cruzcosa@gmail.com>
2020-10-22 23:10:58 -0700

    fix for the game posts request

--- routes/api-game-routes.js
+++ routes/api-game-routes.js
@@ -7,17 +7,12 @@ module.exports = function(app) {
     // console.log(userId);
 
     db.Game.create({
-      // name: req.body.name,
-      name: "zelda",
+      name: req.body.name,
       genre: "game",
-      // published_year: req.body.published_year,
-      published_year: 2007,
-      // link_to_game: req.body.link_to_game,
-      link_to_game: "www.google.com",
-      // link_to_screenshot: req.body.link_to_screenshot,
-      link_to_screenshot: "www.google.com",
-      // userId: userId
-      userId: 1
+      published_year: req.body.published_year,
+      link_to_game: req.body.link_to_game,
+      link_to_screenshot: req.body.link_to_screenshot,
+      UserId: 1
     })
       .then(() => {
         res.end();
