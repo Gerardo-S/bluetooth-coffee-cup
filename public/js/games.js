0dac96edf2735710e22ddc2346ed018f4b0633e4
Shawn Yandall <68927874+dohmr@users.noreply.github.com>
2020-10-22 22:52:45 -0400

    pages re-tooled(users/members) added games results (#43)

--- public/js/games.js
+++ public/js/games.js
@@ -19,16 +19,39 @@ $(document).ready(() => {
       console.log(searchQueryURL);
 
       data.results.forEach(element => {
-        // const newRow = $("<div>").prependTo($("#displayResults"));
-        // newDiv.attr("class", element.id);
-        // const newCol = $("<div>").prependTo($("#displayResults"));
-        // newDiv.attr("id", element.id);
-        // const newP = $("<p>");
-        // newP.addClass("gameBlock");
-        // newP.attr("id", element.id);
-        // newP.text(element.name);
-        // newP.appendTo(newDiv);
+        const newRow = $("<div>").prependTo($("#displayResults"));
+        // newRow.attr("class", "row");
+        const newCol = $("<div>").prependTo(newRow);
+        newCol.attr("class", "col-4");
+        // Card 
+        const newCard = $("<div>").prependTo(newCol);
+        newCard.attr("class", "card border-success mb-3");
+        newCard.attr("style", "max-width: 18rem;");
+        // Card Header
+        const newP = $("<div>");
+        newP.addClass("card-header bg-transparent border-success text-success");
+        newP.attr("id", element.id);
+        newP.text("--> " + element.name);
+        newP.appendTo(newCard);
+        // Card Body
+        const newBody = $("<div>").appendTo(newCard);
+        newBody.addClass("card-body text-success");
+        // Center Image
+        const image = $("<img>").appendTo(newBody);
+        image.attr("src", element.background_image);
+        image.attr("width", 200 + "height", 200);
+        // Card Footer
+        const newFoot = $("<div>").appendTo(newCard);
+        newFoot.addClass("card-footer bg-transparent border-success");
+        // Footer Contents
+        const genre = $("<p>");
+        genre.text("Genre: " + element.tags[0].name);
+        genre.appendTo(newFoot);
+        const release = $("<p>");
+        release.text("Released: " + element.released);
+        release.appendTo(newFoot);
       });
+
 
       // name: element.name,
       // background_image: element.background_image,
