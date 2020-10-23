c2816ed4e32e852e25bf76c04d32745b47c6f606
Chris C <christopher.cruzcosa@gmail.com>
2020-10-22 20:10:48 -0700

    fixed console log error

--- public/js/rawgCalls.js
+++ public/js/rawgCalls.js
@@ -1,40 +1,46 @@
-let baseSearchUrl = "https://api.rawg.io/api/games?key=";
-let apiKey = "72506a506521467da93378cfb7fc9829"
+const baseSearchUrl = "https://api.rawg.io/api/games?key=";
+const apiKey = "72506a506521467da93378cfb7fc9829";
 let searchString;
-const searchQueryURL = baseSearchUrl += apiKey += "&search=" += searchString += "&ordering=-rating&exclude_additions=true";
+const searchQueryURL =
+  baseSearchUrl +
+  apiKey +
+  "&search=" +
+  searchString +
+  "&ordering=-rating&exclude_additions=true";
 
-let baseGameDetailsUrl = "https://api.rawg.io/api/games/";
+const baseGameDetailsUrl = "https://api.rawg.io/api/games/";
 let gameId;
-const gameDetailsURL = baseGameDetailsUrl += gameId += "?key=" += apiKey;
-
+const gameDetailsURL = baseGameDetailsUrl + gameId + "?key=" + apiKey;
 
 function getGameSearchResults(searchString) {
-    searchString = searchString;
+  searchString = searchString;
 
-    $.ajax({
-        url: searchQueryURL,
-        method: "GET",
-      }).then(function (data) {
-        return data
-      })
-      .catch((err) => {
-        console.log(error)
-      })
-};
+  $.ajax({
+    url: searchQueryURL,
+    method: "GET"
+  })
+    .then(function(data) {
+      return data;
+    })
+    .catch(err => {
+      console.log(error);
+    });
+}
 
 function getGameDetails(gameId) {
-    gameId = gameId;
+  gameId = gameId;
 
-    $.ajax({
-        url: gameDetailsURL,
-        method: "GET",
-      }).then(function (data) {
-        return data
-      })
-      .catch((err) => {
-        console.log(error)
-      })
-};
+  $.ajax({
+    url: gameDetailsURL,
+    method: "GET"
+  })
+    .then(function(data) {
+      return data;
+    })
+    .catch(err => {
+      console.log(error);
+    });
+}
 
 exports.getGameSearchResults = getGameSearchResults;
 exports.getGameDetails = getGameDetails;
