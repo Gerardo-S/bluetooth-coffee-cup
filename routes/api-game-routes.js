29821f2d2e21a4ec866807d2e2734f1b13b6e3f8
Gerardo Solis <gerardoa.solis@yahoo.com>
2020-10-23 19:35:01 -0700

    added conditional if user is not logged in they can not save a game

--- routes/api-game-routes.js
+++ routes/api-game-routes.js
@@ -2,21 +2,26 @@ const db = require("../models");
 
 module.exports = function(app) {
   app.post("/api/addgame", (req, res) => {
-    const userId = req.user.id;
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      const userId = req.user.id;
 
-    db.Game.create({
-      name: req.body.name,
-      genre: "game",
-      published_year: req.body.published_year,
-      link_to_game: req.body.link_to_game,
-      link_to_screenshot: req.body.link_to_screenshot,
-      UserId: userId
-    })
-      .then(() => {
-        res.end();
+      db.Game.create({
+        name: req.body.name,
+        genre: "game",
+        published_year: req.body.published_year,
+        link_to_game: req.body.link_to_game,
+        link_to_screenshot: req.body.link_to_screenshot,
+        UserId: userId
       })
-      .catch(err => {
-        res.status(401).json(err);
-      });
+        .then(() => {
+          res.end();
+        })
+        .catch(err => {
+          res.status(401).json(err);
+        });
+    }
   });
 };
