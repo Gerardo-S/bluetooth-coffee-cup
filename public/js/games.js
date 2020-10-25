e18423d45177aad5a729df55a0a483359f4ae5a1
Gerardo Solis <70415527+Gerardo-S@users.noreply.github.com>
2020-10-25 07:13:31 -0700

    Modified button effects for when games are saved and updated delete button (#85)

--- public/js/games.js
+++ public/js/games.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
   var $loading = $("#loading").hide();
   $(document)
-    .ajaxStart(function () {
+    .ajaxStart(function() {
       $loading.show();
     })
-    .ajaxStop(function () {
+    .ajaxStop(function() {
       $loading.hide();
     });
   const params = new URL(document.location).searchParams;
@@ -20,69 +20,85 @@ $(document).ready(() => {
   }
   $.get(searchQueryURL)
     .then(data => {
-      // console.log(data.results);
-      // console.log(searchQueryURL);
-      // console.log(data.results[0]);
-      data.results.forEach(element => {
-        const newRow = $("<div>").appendTo($("#displayResults"));
-        // newRow.attr("class", "row");
-        const newCol = $("<div>").prependTo(newRow);
-        newCol.attr("class", "col");
-        // Card
-        const newCard = $("<div>").prependTo(newCol);
-        newCard.attr("class", "card h-100 text-center border-success mb-3");
-        newCard.attr("style", "max-width: 18rem;");
-        // Card Header
-        const newP = $("<div>");
-        newP.addClass("card-header bg-transparent border-success text-success");
-        newP.attr("id", element.id);
-        newP.text("--> " + element.name);
-        newP.appendTo(newCard);
-        // Card Body
-        const newBody = $("<div>").appendTo(newCard);
-        newBody.addClass("card-body text-success");
-        // Center Image
-        const image = $("<img>").appendTo(newBody);
-        image.attr("src", element.background_image);
-        image.attr("height", 200);
-        image.css({ "object-fit": "contain" });
-        image.addClass("card-img");
-        // Card Footer
-        const newFoot = $("<div>").appendTo(newCard);
-        newFoot.addClass("card-footer bg-transparent border-success");
-        // Save Button
-        const button = $("<button>");
-        button.text("SAVE TO TOP 10");
-        button.attr("class", "save-btn btn btn-dark").appendTo(newCard);
-        button.attr("data-name", element.name);
-        button.attr("data-image", element.background_image);
-        // button.attr("data-genre", element.tags[0].name);
-        button.attr("data-released", element.released);
-        button.attr("data-id", element.id);
-        button.attr("data-link", "https://rawg.io/games/" + element.slug);
-        const save = $("<i>");
-        save.addClass("far fa-save");
-        // <i class="far fa-save"></i>
-        // Footer Contents
-        const genre = $("<p>");
-        // genre.text("Genre: " + element.tags[0].name);
-        genre.appendTo(newFoot);
-        const release = $("<p>");
-        release.text("Released: " + element.released);
-        release.appendTo(newFoot);
-        save.prependTo(button);
-      });
+      appendResults(data);
     })
     .catch(err => {
       console.log(err);
     });
 
+  displayResults();
+});
+
+function appendResults(data) {
+  data.results.forEach(element => {
+    const newRow = $("<div>").appendTo($("#displayResults"));
+    // newRow.attr("class", "row");
+    const newCol = $("<div>").prependTo(newRow);
+    newCol.attr("class", "col");
+    // Card
+    const newCard = $("<div>").prependTo(newCol);
+    newCard.attr("class", "card h-100 text-center border-success mb-3");
+    newCard.attr("style", "max-width: 18rem;");
+    // Card Header
+    const newP = $("<div>");
+    newP.addClass("card-header bg-transparent border-success text-success");
+    newP.attr("id", element.id);
+    newP.text("--> " + element.name);
+    newP.appendTo(newCard);
+    // Card Body
+    const newBody = $("<div>").appendTo(newCard);
+    newBody.addClass("card-body text-success");
+    // Center Image
+    const image = $("<img>").appendTo(newBody);
+    image.attr("src", element.background_image);
+    image.attr("height", 200);
+    image.css({ "object-fit": "contain" });
+    image.addClass("card-img");
+    // Card Footer
+    const newFoot = $("<div>").appendTo(newCard);
+    newFoot.addClass("card-footer bg-transparent border-success");
+    // Save Button
+    const button = $("<button>");
+    button.text("SAVE TO TOP 10");
+    button.attr("class", "save-btn btn btn-dark").appendTo(newCard);
+    button.attr("data-name", element.name);
+    button.attr("data-image", element.background_image);
+    // button.attr("data-genre", element.tags[0].name);
+    button.attr("data-released", element.released);
+    button.attr("data-id", element.id);
+    button.attr("data-link", "https://rawg.io/games/" + element.slug);
+    const save = $("<i>");
+    save.addClass("far fa-save");
+    // <i class="far fa-save"></i>
+    // Footer Contents
+    const genre = $("<p>");
+    // genre.text("Genre: " + element.tags[0].name);
+    genre.appendTo(newFoot);
+    const release = $("<p>");
+    release.text("Released: " + element.released);
+    release.appendTo(newFoot);
+    save.prependTo(button);
+  });
+}
+
+function displayResults() {
   $("#displayResults").on("click", event => {
     event.preventDefault();
+    event.stopPropagation();
+    $(event.target)
+      .removeClass("btn-dark")
+      .addClass("btn-success")
+      .attr("disabled", true);
+    $(event.target)
+      .siblings(".card-body")
+      .children("img")
+      .animate({ height: "0px" });
+    const loginTosaveText = $(event.target)
+      .siblings(".card-body")
+      .text("Login to add and view saved games!")
+      .fadeOut(1);
+    loginTosaveText.fadeIn(500);
 
-    console.log($(this));
-    // $(this).attr("display", "none")
-    // console.log($(event.target).data("link"));
     let gameYear;
 
     if ($(event.target).data("released")) {
@@ -102,10 +118,10 @@ $(document).ready(() => {
       genre: "game"
     })
       .then(() => {
-        console.log("hola");
+        console.log("Game added to playlist!");
       })
       .catch(err => {
         console.log(err);
       });
   });
-});
+}
